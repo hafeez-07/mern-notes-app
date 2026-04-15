7cb3db336cff4eef0d79a180f82ca5c53ec750d0
hafeez-07 <hafeezmohammad689@gmail.com>
2026-04-15 16:06:58 +0530

    fix: update cookie settings for token generation to enhance security

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,8 +5,9 @@ export const generateToken = (userId, res) => {
     expiresIn: "7d",
   });
   res.cookie("token", token, {
-    sameSite: "strict",
+    sameSite: "none",
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
